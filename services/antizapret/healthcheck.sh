e0dfe8b45e21043d0fbfc1453ec210765c91f9cc
Alexander Pankratov <s.pankratoff@gmail.com>
2025-12-29 01:47:02 +0100

    Fix: dont reboot az containers on config changes

--- services/antizapret/healthcheck.sh
+++ services/antizapret/healthcheck.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-DIFF=$( ( cat /root/antizapret/result/* /root/antizapret/config/custom/* | md5sum ) | cmp - /.config_md5 )
-if [ -n "$DIFF" ]; then
+OLD=$( cat /.config_md5 )
+NEW=$( cat /root/antizapret/result/* /root/antizapret/config/custom/* | md5sum )
+if [[ "$OLD" != "$NEW" ]]; then
     echo "config files changed"
     doall
     curl -s "http://127.0.0.1/update/"
