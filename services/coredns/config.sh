ecf0fd1a4d85d7486261ad443602343ab00180fc
Alexander Pankratov <s.pankratoff@gmail.com>
2026-01-03 20:39:59 +0100

    Feat: add ip validation to coredns and adguard containers

--- services/coredns/config.sh
+++ services/coredns/config.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-export AZ_LOCAL_HOST=$(dig +short az-local | head -n1)
-export AZ_WORLD_HOST=$(dig +short az-world | head -n1)
-export DNS_HOST=$(dig +short adguard | head -n1)
+# resolve domain address to ip address
+function resolve () {
+    # $1 domain/ip address, $2 fallback ip address
+    res="$(dig +short $1 | head -n1)"
+    if [[ "$res" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        echo "$res"
+    else
+        echo "$2"
+    fi
+}
+
+export AZ_LOCAL_HOST=$(resolve 'az-local' '')
+export AZ_WORLD_HOST=$(resolve 'az-world' '')
+export DNS_HOST=$(resolve 'adguard' '')
 
 envsubst < /root/Corefile.template > /Corefile
