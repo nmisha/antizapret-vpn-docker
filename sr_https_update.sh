775fac658c525e65afc422c8414eac2c4eae4f45
Mikhail <tyou@mail.ru>
2026-01-18 21:42:33 +0300

    upd

--- sr_https_update.sh
+++ sr_https_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./fix_git.sh
+./sr_git_pull.sh
 
-docker compose up -d --no-deps --force-recreate --build proxy
+docker compose up -d --no-deps --force-recreate --build https
 docker compose up -d --no-deps --force-recreate --build dashboard
 docker compose up -d --no-deps --force-recreate --build authelia
 
