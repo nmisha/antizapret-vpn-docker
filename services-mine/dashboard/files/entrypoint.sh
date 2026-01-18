10ddd73215e41bd706863bb96dedd352897b1807
Mikhail <tyou@mail.ru>
2026-01-18 20:31:20 +0300

    upd

--- services-mine/dashboard/files/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-/init.sh
-
-lighttpd -D -f /etc/lighttpd/lighttpd.conf
--- services-mine/proxy/files/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-/init.sh
-
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
