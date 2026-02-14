1bf94bdf9b9893e726e3d4a8bdf39e473890dd96
Mikhail <tyou@mail.ru>
2026-02-14 22:42:45 +0300

    script upd

--- sr_swarm_start.sh
+++ sr_swarm_start.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 
-docker compose config | docker run --rm -i xtrime/antizapret-vpn:5 compose2swarm | docker stack deploy --prune -c - antizapret
+docker compose config | docker run --rm -i xtrime/antizapret-vpn:6 compose2swarm | docker stack deploy --prune -c - antizapret
