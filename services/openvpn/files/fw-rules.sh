2cef3a6b39aff2d0cb6ac10219bf6dab6acd4da2
Mikhail <tyou@mail.ru>
2026-04-13 21:02:22 +0300

    REVERT routes for block WG OVPN

--- services/openvpn/files/fw-rules.sh
+++ services/openvpn/files/fw-rules.sh
@@ -8,7 +8,6 @@ DOCKER_SUBNET="$(ipcalc "$(ip -4 addr show dev eth0 | awk '$1=="inet" {print $2;
 cat << EOF | sponge /etc/environment
 OPENVPN_LOCAL_IP_RANGE='${OPENVPN_LOCAL_IP_RANGE:-"10.1.165.0"}'
 OPENVPN_DNS='${OPENVPN_DNS:-"14.16.0.1"}'
-OPENVPN_ENABLE_DEST_FILTER='${OPENVPN_ENABLE_DEST_FILTER:-"true"}'
 AZ_SUBNET=${AZ_SUBNET:-"14.16.0.0/14"}
 DOCKER_SUBNET=${DOCKER_SUBNET}
 NIC='$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)'
@@ -17,24 +16,6 @@ EOF
 source /etc/environment
 ln -sf /etc/environment /etc/profile.d/environment.sh
 
-OPENVPN_ALLOWED_IPS="${OPENVPN_LOCAL_IP_RANGE}/24,${AZ_SUBNET},${DOCKER_SUBNET}"
-if [ -f /opt/antizapret/result/openvpn-blocked-ranges.txt ]; then
-    blocked_ranges=$(grep -v '^[[:space:]]*$' /opt/antizapret/result/openvpn-blocked-ranges.txt | tr '\n' ',' | sed 's/,$//')
-    if [ -n "${blocked_ranges}" ]; then
-        OPENVPN_ALLOWED_IPS="${OPENVPN_ALLOWED_IPS},${blocked_ranges}"
-    fi
-fi
-
-build_forward_allow_rules() {
-    local cidr
-    while IFS= read -r cidr; do
-        [ -z "$cidr" ] && continue
-        printf 'iptables -A ovpn_allowed_destinations -d %s -j ACCEPT;\n' "$cidr"
-    done < <(printf '%s\n' "$OPENVPN_ALLOWED_IPS" | tr ',' '\n' | sed 's/^[[:space:]]*//; s/[[:space:]]*$//' | sed '/^$/d')
-}
-
-FORWARD_ALLOW_RULES="$(build_forward_allow_rules)"
-
 iptables -t nat -N masq_not_local;
 iptables -t nat -A POSTROUTING -s ${OPENVPN_LOCAL_IP_RANGE}/24 -j masq_not_local;
 iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -p tcp --dport 53 -j RETURN;
@@ -42,11 +23,5 @@ iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -p udp --dport 53 -j RETUR
 iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -j MASQUERADE;
 iptables -t nat -A masq_not_local -d ${AZ_SUBNET} -j RETURN;
 iptables -t nat -A masq_not_local -j MASQUERADE;
-if [ "${OPENVPN_ENABLE_DEST_FILTER}" = "true" ]; then
-    iptables -N ovpn_allowed_destinations;
-    iptables -I FORWARD 1 -s ${OPENVPN_LOCAL_IP_RANGE}/24 -j ovpn_allowed_destinations;
-    iptables -A ovpn_allowed_destinations -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT;
-    ${FORWARD_ALLOW_RULES}iptables -A ovpn_allowed_destinations -j DROP;
-fi
 
 ./routes.sh --vpn --dns-file /opt/antizapret/result/dns.txt &
