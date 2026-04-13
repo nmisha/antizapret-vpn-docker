fd7d9e7785c7d3210b99ec1ad9a6f29f1fcc9a91
Mikhail <tyou@mail.ru>
2026-04-13 11:43:46 +0300

    OpenVPN restriction

--- services/openvpn/files/fw-rules.sh
+++ services/openvpn/files/fw-rules.sh
@@ -16,6 +16,24 @@ EOF
 source /etc/environment
 ln -sf /etc/environment /etc/profile.d/environment.sh
 
+OPENVPN_ALLOWED_IPS="${OPENVPN_LOCAL_IP_RANGE}/24,${AZ_SUBNET},${DOCKER_SUBNET}"
+if [ -f /opt/antizapret/result/openvpn-blocked-ranges.txt ]; then
+    blocked_ranges=$(grep -v '^[[:space:]]*$' /opt/antizapret/result/openvpn-blocked-ranges.txt | tr '\n' ',' | sed 's/,$//')
+    if [ -n "${blocked_ranges}" ]; then
+        OPENVPN_ALLOWED_IPS="${OPENVPN_ALLOWED_IPS},${blocked_ranges}"
+    fi
+fi
+
+build_forward_allow_rules() {
+    local cidr
+    while IFS= read -r cidr; do
+        [ -z "$cidr" ] && continue
+        printf 'iptables -A ovpn_allowed_destinations -d %s -j ACCEPT;\n' "$cidr"
+    done < <(echo "$OPENVPN_ALLOWED_IPS" | tr ',' '\n' | awk '{gsub(/^[[:space:]]+|[[:space:]]+$/, ""); if (length) print}')
+}
+
+FORWARD_ALLOW_RULES="$(build_forward_allow_rules)"
+
 iptables -t nat -N masq_not_local;
 iptables -t nat -A POSTROUTING -s ${OPENVPN_LOCAL_IP_RANGE}/24 -j masq_not_local;
 iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -p tcp --dport 53 -j RETURN;
@@ -23,5 +41,9 @@ iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -p udp --dport 53 -j RETUR
 iptables -t nat -A masq_not_local -d ${DOCKER_SUBNET} -j MASQUERADE;
 iptables -t nat -A masq_not_local -d ${AZ_SUBNET} -j RETURN;
 iptables -t nat -A masq_not_local -j MASQUERADE;
+iptables -N ovpn_allowed_destinations;
+iptables -I FORWARD 1 -s ${OPENVPN_LOCAL_IP_RANGE}/24 -j ovpn_allowed_destinations;
+iptables -A ovpn_allowed_destinations -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT;
+${FORWARD_ALLOW_RULES}iptables -A ovpn_allowed_destinations -j DROP;
 
 ./routes.sh --vpn --dns-file /opt/antizapret/result/dns.txt &
