2f7f7485a2c976bf47befa21558ec9a9733c0713
Mikhail <tyou@mail.ru>
2026-08-18 18:24:26 +0300

    upd starter

--- sr_swarm_start.sh
+++ sr_swarm_start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker compose config | docker run --rm -i xtrime/antizapret-vpn:6 compose2swarm | docker stack deploy --prune -c - antizapret
+# docker compose config | docker run --rm -i xtrime/antizapret-vpn:6 compose2swarm | docker stack deploy --prune -c - antizapret
+
+docker compose --env-file compose.swarm.env config | docker run --pull always --rm -i xtrime/antizapret-vpn:6 compose2swarm | docker stack deploy --prune -c - antizapret
+
